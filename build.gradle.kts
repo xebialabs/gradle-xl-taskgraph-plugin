8d44bea8e587a979307a4a23d5fa57b41f6e1b95
bnechyporenko <bogdannechiporenko@gmail.com>
2021-12-16 15:46:11 +0100

    ENG-6757 Use Nebula release plugin for Gradle plugins

---
+++ build.gradle.kts
@@ -0,0 +1,71 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+plugins {
+    kotlin("jvm") version "1.4.20"
+
+    id("groovy")
+    id("idea")
+    id("maven-publish")
+    id("nebula.release") version "15.3.1"
+}
+
+group = "com.xebialabs.gradle.plugins"
+project.defaultTasks = listOf("build")
+
+val releasedVersion = "2.0.0-${LocalDateTime.now().format(DateTimeFormatter.ofPattern("Mdd.Hmm"))}"
+project.extra.set("releasedVersion", releasedVersion)
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    gradlePluginPortal()
+    maven {
+        url = uri("https://plugins.gradle.org/m2/")
+    }
+    maven {
+        url = uri("https://oss.sonatype.org/service/local/repositories/releases/content")
+    }
+    maven {
+        credentials {
+            username = project.property("nexusUserName").toString()
+            password = project.property("nexusPassword").toString()
+        }
+        url = uri("${project.property("nexusBaseUrl")}/repositories/releases")
+    }
+}
+
+idea {
+    module {
+        setDownloadJavadoc(true)
+        setDownloadSources(true)
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+    withSourcesJar()
+    withJavadocJar()
+}
+
+dependencies {
+    implementation(gradleApi())
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            url = uri("${project.property("nexusBaseUrl")}/repositories/releases")
+            credentials {
+                username = project.property("nexusUserName").toString()
+                password = project.property("nexusPassword").toString()
+            }
+        }
+    }
+}
